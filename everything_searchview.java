3a3adb6c4ed065467a6509729c577ffe2b1463ce
Anurag Bhalekar <44122742+anurag-176@users.noreply.github.com>
2020-10-22 13:05:43 +0530

    Create everything_searchview.java
    Initialize, expand, animate and fit to screen width.

---
+++ everything_searchview.java
@@ -0,0 +1,25 @@
+//Everything for SearchView in Java
+
+//Supported SearchView Library
+import android.widget.SearchView;
+
+//Global
+public static SearchView searchView;
+
+//Initialization
+searchView = findViewById(R.id.searchView);
+
+//SearchView hint
+searchView.setQueryHint("Google Search");
+
+//SearchView smooth expand collapse animation
+int id = searchView.getContext().getResources().getIdentifier("android:id/search_src_text", null, null);
+TextView searchText = (TextView) searchView.findViewById(id);
+Typeface myCustomFont = Typeface.createFromAsset(getAssets(),"font/montserrat_medium.ttf");
+searchText.setTypeface(myCustomFont);
+final int searchBarId = searchView.getContext().getResources().getIdentifier("android:id/search_bar", null, null);  // Remove this line
+LinearLayout searchBar = (LinearLayout) searchView.findViewById(searchBarId);
+searchBar.setLayoutTransition(new LayoutTransition());
+
+//SearchView width match_parent
+searchView.setMaxWidth( Integer.MAX_VALUE );
